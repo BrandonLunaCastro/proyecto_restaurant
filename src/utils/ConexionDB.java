e22836e0ee43d4ad559fc28273e0bc5b4e33e69b
Brandon <blunacastro2003@gmail.com>
2026-05-10 00:43:10 -0300

    Agregado el conector a Mysql y adaptado los modulos principales

---
+++ src/utils/ConexionDB.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+
+    private static final String URL  = "jdbc:mysql://localhost:3306/restaurante";
+    private static final String USER = "root";
+    private static final String PASS = "0343";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
